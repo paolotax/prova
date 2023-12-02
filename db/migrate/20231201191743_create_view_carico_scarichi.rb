b3a2caf8db463c1f7ac0c599a4688ca7ab6a7760
Paolo Tassinari <paolo.tassinari@gmail.com>
2023-12-02 08:50:20 +0100

    created Views::Riga and sql

--- db/migrate/20231201191743_create_view_carico_scarichi.rb
+++
@@ -1,5 +0,0 @@
-class CreateViewCaricoScarichi < ActiveRecord::Migration[7.1]
-  def change
-    create_view :view_carico_scarichi
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_01_191743) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_02_073600) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,22 +47,56 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_191743) do
     GROUP BY fornitore, tipo_documento, numero_documento, data_documento, totale_documento
     ORDER BY fornitore, data_documento, numero_documento, tipo_documento;
   SQL
-  create_view "view_carico_scarichi", sql_definition: <<-SQL
-      SELECT imports.codice_articolo,
-      imports.descrizione,
-      (- sum(imports.quantita)) AS quantita_totale,
-      (- sum(imports.importo_netto)) AS importo_netto_totale
-     FROM imports
-    WHERE ((imports.tipo_documento)::text = 'Nota di accredito'::text)
-    GROUP BY imports.codice_articolo, imports.descrizione
-  UNION ALL
-   SELECT imports.codice_articolo,
-      imports.descrizione,
-      sum(imports.quantita) AS quantita_totale,
-      sum(imports.importo_netto) AS importo_netto_totale
-     FROM imports
-    WHERE ((imports.tipo_documento)::text <> 'Nota di accredito'::text)
-    GROUP BY imports.codice_articolo, imports.descrizione
-    ORDER BY 1;
+  create_view "view_name", sql_definition: <<-SQL
+      SELECT id,
+      fornitore,
+      tipo_documento,
+      numero_documento,
+      data_documento,
+          CASE
+              WHEN ((tipo_documento)::text = 'Nota di accredito'::text) THEN (- totale_documento)
+              ELSE totale_documento
+          END AS totale_documento,
+      riga,
+      codice_articolo,
+      descrizione,
+      prezzo_unitario,
+          CASE
+              WHEN ((tipo_documento)::text = 'Nota di accredito'::text) THEN (- quantita)
+              ELSE quantita
+          END AS quantita,
+          CASE
+              WHEN ((tipo_documento)::text = 'Nota di accredito'::text) THEN (- importo_netto)
+              ELSE importo_netto
+          END AS importo_netto,
+      sconto,
+      iva
+     FROM imports;
+  SQL
+  create_view "view_righe", sql_definition: <<-SQL
+      SELECT id,
+      fornitore,
+      tipo_documento,
+      numero_documento,
+      data_documento,
+          CASE
+              WHEN ((tipo_documento)::text = 'Nota di accredito'::text) THEN (- totale_documento)
+              ELSE totale_documento
+          END AS totale_documento,
+      riga,
+      codice_articolo,
+      descrizione,
+      prezzo_unitario,
+          CASE
+              WHEN ((tipo_documento)::text = 'Nota di accredito'::text) THEN (- quantita)
+              ELSE quantita
+          END AS quantita,
+          CASE
+              WHEN ((tipo_documento)::text = 'Nota di accredito'::text) THEN (- importo_netto)
+              ELSE importo_netto
+          END AS importo_netto,
+      sconto,
+      iva
+     FROM imports;
   SQL
 end
