139cbff6730c234ebe747b4ede1bea32ce17a794
Paolo Tassinari <paolo.tassinari@gmail.com>
2023-12-08 14:18:23 +0100

    show documento e righe

---
+++ app/views/documenti/_documento_table.rb
@@ -0,0 +1,147 @@
+
+
+    <tr>
+        <td class="relative py-5 pr-6">
+            <div class="flex gap-x-6">
+                <div class="flex-auto">
+                <div class="flex items-start gap-x-3">
+                    <div class="text-sm font-medium leading-6 text-gray-900">
+                        $7,600.00 USD
+                        </div>
+                    <div class="rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset text-green-700 bg-green-50 ring-green-600/20">
+                        Paid
+                    </div>
+                </div>
+                <div class="mt-1 text-xs leading-5 text-gray-500">$500.00 tax</div>
+                </div>
+            </div>
+        <div class="absolute bottom-0 right-full h-px w-screen bg-gray-100"></div>
+        <div class="absolute bottom-0 left-0 h-px w-screen bg-gray-100"></div>
+        </td>
+        <td class="hidden py-5 pr-6 sm:table-cell">
+        <div class="text-sm leading-6 text-gray-900">Reform</div>
+        <div class="mt-1 text-xs leading-5 text-gray-500">Website redesign</div>
+        </td>
+        <td class="py-5 text-right">
+        <div class="flex justify-end">
+            <a href="#" class="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500">View<span class="hidden sm:inline"> transaction</span><span class="sr-only">, invoice #00012, Reform</span></a>
+        </div>
+        <div class="mt-1 text-xs leading-5 text-gray-500">Invoice <span class="text-gray-900">#00012</span></div>
+        </td>
+    </tr>
+    <tr>
+        <td class="relative py-5 pr-6">
+        <div class="flex gap-x-6">
+            <svg class="hidden h-6 w-5 flex-none text-gray-400 sm:block" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm.75-11.25a.75.75 0 00-1.5 0v4.59L7.3 9.24a.75.75 0 00-1.1 1.02l3.25 3.5a.75.75 0 001.1 0l3.25-3.5a.75.75 0 10-1.1-1.02l-1.95 2.1V6.75z" clip-rule="evenodd" />
+            </svg>
+            <div class="flex-auto">
+            <div class="flex items-start gap-x-3">
+                <div class="text-sm font-medium leading-6 text-gray-900">$10,000.00 USD</div>
+                <div class="rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset text-gray-600 bg-gray-50 ring-gray-500/10">Withdraw</div>
+            </div>
+            </div>
+        </div>
+        <div class="absolute bottom-0 right-full h-px w-screen bg-gray-100"></div>
+        <div class="absolute bottom-0 left-0 h-px w-screen bg-gray-100"></div>
+        </td>
+        <td class="hidden py-5 pr-6 sm:table-cell">
+        <div class="text-sm leading-6 text-gray-900">Tom Cook</div>
+        <div class="mt-1 text-xs leading-5 text-gray-500">Salary</div>
+        </td>
+        <td class="py-5 text-right">
+        <div class="flex justify-end">
+            <a href="#" class="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500">View<span class="hidden sm:inline"> transaction</span><span class="sr-only">, invoice #00011, Tom Cook</span></a>
+        </div>
+        <div class="mt-1 text-xs leading-5 text-gray-500">Invoice <span class="text-gray-900">#00011</span></div>
+        </td>
+    </tr>
+    <tr>
+        <td class="relative py-5 pr-6">
+        <div class="flex gap-x-6">
+            <svg class="hidden h-6 w-5 flex-none text-gray-400 sm:block" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd" d="M15.312 11.424a5.5 5.5 0 01-9.201 2.466l-.312-.311h2.433a.75.75 0 000-1.5H3.989a.75.75 0 00-.75.75v4.242a.75.75 0 001.5 0v-2.43l.31.31a7 7 0 0011.712-3.138.75.75 0 00-1.449-.39zm1.23-3.723a.75.75 0 00.219-.53V2.929a.75.75 0 00-1.5 0V5.36l-.31-.31A7 7 0 003.239 8.188a.75.75 0 101.448.389A5.5 5.5 0 0113.89 6.11l.311.31h-2.432a.75.75 0 000 1.5h4.243a.75.75 0 00.53-.219z" clip-rule="evenodd" />
+            </svg>
+            <div class="flex-auto">
+            <div class="flex items-start gap-x-3">
+                <div class="text-sm font-medium leading-6 text-gray-900">$2,000.00 USD</div>
+                <div class="rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset text-red-700 bg-red-50 ring-red-600/10">Overdue</div>
+            </div>
+            <div class="mt-1 text-xs leading-5 text-gray-500">$130.00 tax</div>
+            </div>
+        </div>
+        <div class="absolute bottom-0 right-full h-px w-screen bg-gray-100"></div>
+        <div class="absolute bottom-0 left-0 h-px w-screen bg-gray-100"></div>
+        </td>
+        <td class="hidden py-5 pr-6 sm:table-cell">
+        <div class="text-sm leading-6 text-gray-900">Tuple</div>
+        <div class="mt-1 text-xs leading-5 text-gray-500">Logo design</div>
+        </td>
+        <td class="py-5 text-right">
+        <div class="flex justify-end">
+            <a href="#" class="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500">View<span class="hidden sm:inline"> transaction</span><span class="sr-only">, invoice #00009, Tuple</span></a>
+        </div>
+        <div class="mt-1 text-xs leading-5 text-gray-500">Invoice <span class="text-gray-900">#00009</span></div>
+        </td>
+    </tr>
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<td class="flex justify-between gap-x-6 py-5">
+  <div class="flex min-w-0 gap-x-4">
+    <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+    <div class="min-w-0 flex-auto">
+      <p class="text-sm font-semibold leading-6 text-gray-900">
+        <%= documento.fornitore %>
+      </p>
+      <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+        
+      </p>
+      <p class="mt-1 truncate text-xs leading-5 text-gray-500">
+        <%= documento.tipo_documento %> 
+      </p>
+      <p class="mt-1 truncate text-xs leading-5 text-gray-500">  
+        <span class="text-gray-900 font-semibold">
+          <%= link_to documento.numero_documento, documento_path(documento) %>
+        </span>
+        del <%= documento.data_documento.strftime('%d-%m-%Y') %>  
+      </p>
+    </div>
+  </div>
+  <div class="shrink-0 text-right sm:flex sm:flex-col sm:items-end">
+    <% if not  documento.totale_documento.nil? %>
+    
+      <% if documento.totale_documento > 0 %>
+        <p class="text-indigo-500" >
+      <% else %>
+        <p class="text-red-500" >
+      <% end %>
+    <% end %>
+    
+    <%= number_to_currency documento.totale_documento, locale: :it %>
+    </p>
+    
+    <p class="text-sm leading-6 text-gray-900">
+      
+       <%= documento.quantita_totale %>
+       copie
+    </p>  
+    <p class="mt-1 text-xs leading-5">
+
+    </p>
+  </div>      
+</li>
