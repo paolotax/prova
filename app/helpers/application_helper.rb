319d8189ffaa0781fc54c41921d2e58ba31dc77d
paolotax <paolo.tassinari@gmail.com>
2023-12-18 17:59:01 +0100

    Refactor application layout and add embedded SVG helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,6 +9,22 @@ module ApplicationHelper
 
   end
 
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    asset = assets.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      svg["class"] = options[:class] if options[:class].present?
+    else
+      doc = "<!-- SVG #{filename} not found -->"
+    end
+
+    raw doc
+  end
+
 # stack overflow 70960161 examle
 
 #   # in the contracts_controller.rb
