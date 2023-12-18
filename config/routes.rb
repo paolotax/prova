a8db590e8fff6f24f638051284e3a4476d9c8f88
paolotax <paolo.tassinari@gmail.com>
2023-12-18 15:55:54 +0100

    Update routes and remove search controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "documenti#index"
+  root "articoli#index"
 end
