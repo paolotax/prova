7efed26b30c2be5466a0d159c96ffe18563476f0
paolotax <paolo.tassinari@gmail.com>
2023-12-28 14:44:40 +0100

    update schema information

--- app/models/import_scuola.rb
+++ app/models/import_scuola.rb
@@ -1,2 +1,30 @@
+# == Schema Information
+#
+# Table name: import_scuole
+#
+#  id                                        :bigint           not null, primary key
+#  ANNOSCOLASTICO                            :string
+#  AREAGEOGRAFICA                            :string
+#  REGIONE                                   :string
+#  PROVINCIA                                 :string
+#  CODICEISTITUTORIFERIMENTO                 :string
+#  DENOMINAZIONEISTITUTORIFERIMENTO          :string
+#  CODICESCUOLA                              :string
+#  DENOMINAZIONESCUOLA                       :string
+#  INDIRIZZOSCUOLA                           :string
+#  CAPSCUOLA                                 :string
+#  CODICECOMUNESCUOLA                        :string
+#  DESCRIZIONECOMUNE                         :string
+#  DESCRIZIONECARATTERISTICASCUOLA           :string
+#  DESCRIZIONETIPOLOGIAGRADOISTRUZIONESCUOLA :string
+#  INDICAZIONESEDEDIRETTIVO                  :string
+#  INDICAZIONESEDEOMNICOMPRENSIVO            :string
+#  INDIRIZZOEMAILSCUOLA                      :string
+#  INDIRIZZOPECSCUOLA                        :string
+#  SITOWEBSCUOLA                             :string
+#  SEDESCOLASTICA                            :string
+#  created_at                                :datetime         not null
+#  updated_at                                :datetime         not null
+#
 class ImportScuola < ApplicationRecord
 end
--- test/models/import_scuola_test.rb
+++ test/models/import_scuola_test.rb
@@ -1,3 +1,31 @@
+# == Schema Information
+#
+# Table name: import_scuole
+#
+#  id                                        :bigint           not null, primary key
+#  ANNOSCOLASTICO                            :string
+#  AREAGEOGRAFICA                            :string
+#  REGIONE                                   :string
+#  PROVINCIA                                 :string
+#  CODICEISTITUTORIFERIMENTO                 :string
+#  DENOMINAZIONEISTITUTORIFERIMENTO          :string
+#  CODICESCUOLA                              :string
+#  DENOMINAZIONESCUOLA                       :string
+#  INDIRIZZOSCUOLA                           :string
+#  CAPSCUOLA                                 :string
+#  CODICECOMUNESCUOLA                        :string
+#  DESCRIZIONECOMUNE                         :string
+#  DESCRIZIONECARATTERISTICASCUOLA           :string
+#  DESCRIZIONETIPOLOGIAGRADOISTRUZIONESCUOLA :string
+#  INDICAZIONESEDEDIRETTIVO                  :string
+#  INDICAZIONESEDEOMNICOMPRENSIVO            :string
+#  INDIRIZZOEMAILSCUOLA                      :string
+#  INDIRIZZOPECSCUOLA                        :string
+#  SITOWEBSCUOLA                             :string
+#  SEDESCOLASTICA                            :string
+#  created_at                                :datetime         not null
+#  updated_at                                :datetime         not null
+#
 require "test_helper"
 
 class ImportScuolaTest < ActiveSupport::TestCase
