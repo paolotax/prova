e203d2e132ac85ef42b5c918fd4145cdf6500ba7
paolotax <paolo.tassinari@gmail.com>
2024-02-13 22:40:59 +0100

    Refactor table filtering logic and add group selection feature

--- app/javascript/controllers/indice_tabella_controller.js
+++ app/javascript/controllers/indice_tabella_controller.js
@@ -5,4 +5,34 @@ export default class extends Controller {
   connect() {
     console.log("Connected to indice-tabella")
   }
+
+  changeGroup(event) {
+    
+    // Get the selected value
+    const selectedValue = event.target.value
+    console.log(selectedValue)
+
+    // Get the table rows
+    const rows = this.element.querySelectorAll("tbody tr")
+    console.log(rows.length)
+
+    // Loop through the rows
+    rows.forEach(row => {
+      // Get the group value from the row
+      const group = row.dataset.group
+      console.log(group === selectedValue)
+
+      // If the selected value is "all" or the group value matches the selected value, show the row
+      if (selectedValue === "all" || group === selectedValue) {
+        // row.classList.remove("hidden");
+        row.style.display = "table-row"
+      } else {
+        // Otherwise, hide the row
+        // row.classList.add("hidden");
+        row.style.display = "none"
+      }
+    })
+  }
+    
+
 }
