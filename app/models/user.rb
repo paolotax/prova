85d84c6ce0058131d5c6b88b0b5ed873dfb0d9ea
paolotax <paolo.tassinari@gmail.com>
2024-02-19 16:03:26 +0100

    Commented out user validations and password encryption for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,12 +18,10 @@ class User < ApplicationRecord
   has_one_attached :avatar
   has_rich_text :card
 
-  has_secure_password
-
-  validates :name,  presence: true, uniqueness: { case_sensitive: false }
-  validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
-  validates :partita_iva, format: { with: /\A\d{11}\z/ }
-  validates :password, length: { minimum: 6, allow_blank: true }
+  #validates :name,  presence: true, uniqueness: { case_sensitive: false }
+  #validates :email, format: { with: /\S+@\S+/ }, uniqueness: { case_sensitive: false }
+  #validates :partita_iva, format: { with: /\A\d{11}\z/ }
+  #validates :password, length: { minimum: 6, allow_blank: true }
 
   has_many :user_scuole, dependent: :destroy  
   has_many :import_scuole, through: :user_scuole
