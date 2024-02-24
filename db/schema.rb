9505269f286333846b1d5f02b9bab1bab802dd52
paolotax <paolo.tassinari@gmail.com>
2024-02-24 13:23:07 +0100

    Update Procfile.dev and schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_22_134152) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_121929) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,7 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_22_134152) do
   end
 
   create_table "appunti", force: :cascade do |t|
-    t.bigint "import_scuola_id", null: false
+    t.bigint "import_scuola_id"
     t.bigint "user_id", null: false
     t.bigint "import_adozione_id"
     t.string "nome"
