a0f88173f662e6f970b6ae9ba097a5713561535c
paolotax <paolo.tassinari@gmail.com>
2024-02-25 08:50:39 +0100

    Add Cropper.js library and its dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -11,3 +11,15 @@ pin "debounce", to: "https://ga.jspm.io/npm:debounce@2.0.0/index.js"
 pin "tailwindcss-stimulus-components", to: "https://ga.jspm.io/npm:tailwindcss-stimulus-components@4.0.4/dist/tailwindcss-stimulus-components.module.js"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
+pin "cropperjs" # @2.0.0
+pin "@cropper/element", to: "@cropper--element.js" # @2.0.0
+pin "@cropper/element-canvas", to: "@cropper--element-canvas.js" # @2.0.0
+pin "@cropper/element-crosshair", to: "@cropper--element-crosshair.js" # @2.0.0
+pin "@cropper/element-grid", to: "@cropper--element-grid.js" # @2.0.0
+pin "@cropper/element-handle", to: "@cropper--element-handle.js" # @2.0.0
+pin "@cropper/element-image", to: "@cropper--element-image.js" # @2.0.0
+pin "@cropper/element-selection", to: "@cropper--element-selection.js" # @2.0.0
+pin "@cropper/element-shade", to: "@cropper--element-shade.js" # @2.0.0
+pin "@cropper/element-viewer", to: "@cropper--element-viewer.js" # @2.0.0
+pin "@cropper/elements", to: "@cropper--elements.js" # @2.0.0
+pin "@cropper/utils", to: "@cropper--utils.js" # @2.0.0
