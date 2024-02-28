c3250b4781df4418acd331157ca7f274d8c6eab0
paolotax <paolo.tassinari@gmail.com>
2024-02-28 19:26:39 +0100

    Update configuration and code for secure context

--- app/javascript/controllers/tax_trix_clipboard_controller.js
+++ app/javascript/controllers/tax_trix_clipboard_controller.js
@@ -4,20 +4,38 @@ import Clipboard from 'stimulus-clipboard';
 export default class extends Clipboard {
   connect() {
     super.connect()
+    if (window.isSecureContext) {
+      console.log('The context is secure, can use navigator.clipboard');
+    } else {
+      console.log('The context is NOT secure BARBUN');
+      this.buttonTarget.classList.add("hidden");
+    }
   }
   
   copy(e) {
     e.preventDefault();
-    const s = this.sourceTarget.querySelector("div").textContent || this.sourceTarget.value;
-    navigator.clipboard.writeText(s).then((()=>this.copied()))
+    const s = this.sourceTarget.querySelector("div").innerHTML || "niente da fare non funziona";
+    console.log(s);
+    navigator.clipboard
+      .writeText(s)
+      .then((() => this.copied()))
+      .catch(() => { alert("something went wrong");});
   }
 
   // Function to override when to input is copied.
   copied() {
     this.hasButtonTarget && (this.timeout && clearTimeout(this.timeout),
     this.buttonTarget.innerHTML = this.successContentValue,
+    this.buttonTarget.classList.remove("bg-yellow-300"),
+    this.buttonTarget.classList.add("bg-cyan-500"),
+    this.buttonTarget.classList.remove("text-yellow-800"),
+    this.buttonTarget.classList.add("text-white"),
     this.timeout = setTimeout((()=>{
         this.buttonTarget.innerHTML = this.originalContent
+        this.buttonTarget.classList.add("bg-yellow-300"),
+        this.buttonTarget.classList.remove("bg-cyan-500"),
+        this.buttonTarget.classList.add("text-yellow-800"),
+        this.buttonTarget.classList.remove("text-white")
     }
     ), this.successDurationValue))
   }
