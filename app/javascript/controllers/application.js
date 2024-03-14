504d2748784b68919c0cdee1c8c1856e57916f64
paolotax <paolo.tassinari@gmail.com>
2024-03-14 07:04:31 +0100

    Add Slideover component to application.js and update click actions in _main_menu.html.erb

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,11 +1,12 @@
 import { Application } from "@hotwired/stimulus"
 import Clipboard from 'stimulus-clipboard'
 import CheckboxSelectAll from 'stimulus-checkbox-select-all'
-import { Tabs } from 'tailwindcss-stimulus-components'
+import { Tabs, Slideover } from 'tailwindcss-stimulus-components'
 
 const application = Application.start()
 application.register('clipboard', Clipboard)
 application.register('tabs', Tabs)
+application.register('slideover', Slideover)
 application.register('checkbox-select-all', CheckboxSelectAll)
 
 // Configure Stimulus development experience
