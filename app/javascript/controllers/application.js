66ee99760b6e569ae064febdcb2dcea7b12eacfd
paolotax <paolo.tassinari@gmail.com>
2024-03-18 07:10:22 +0100

    Add dropdown functionality and remove tax dropdown controller

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,13 @@
 import { Application } from "@hotwired/stimulus"
 import Clipboard from 'stimulus-clipboard'
 import CheckboxSelectAll from 'stimulus-checkbox-select-all'
-import { Tabs, Slideover } from 'tailwindcss-stimulus-components'
+import { Tabs, Slideover, Dropdown } from 'tailwindcss-stimulus-components'
 
 const application = Application.start()
 application.register('clipboard', Clipboard)
 application.register('tabs', Tabs)
 application.register('slideover', Slideover)
+application.register('dropdown', Dropdown)
 application.register('checkbox-select-all', CheckboxSelectAll)
 
 // Configure Stimulus development experience
