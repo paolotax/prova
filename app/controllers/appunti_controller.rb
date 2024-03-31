2c21faa09fc535d2f360e9df939940bbe1c0c0a3
paolotax <paolo.tassinari@gmail.com>
2024-03-31 15:21:24 +0200

    Refactor appunti_controller.rb and import_scuole/_scuola_small.html.erb, and update application.html.erb

--- app/controllers/appunti_controller.rb
+++ app/controllers/appunti_controller.rb
@@ -5,7 +5,8 @@ class AppuntiController < ApplicationController
   before_action :ensure_frame_response, only: %i[ new edit show ]
 
   def index
-      @appunti = current_user.appunti.order(created_at: :desc)       
+      @appunti = current_user.appunti.order(created_at: :desc)
+      @appunti = @appunti.non_archiviati if params[:filter] == "non_archiviati"       
       @appunti = @appunti.search_all_word(params[:search]) if params[:search] && !params[:search].blank?     
       @appunti = @appunti.search(params[:q]) if params[:q]
       @pagy, @appunti =  pagy(@appunti.all, items: 30)
