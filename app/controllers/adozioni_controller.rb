219546effc176a2046e0d0c26eca002a168ba05e
paolotax <paolo.tassinari@gmail.com>
2024-05-11 06:41:34 +0200

    Refactor UI layout and fix bug in AdozioniController

--- app/controllers/adozioni_controller.rb
+++ app/controllers/adozioni_controller.rb
@@ -9,7 +9,9 @@ class AdozioniController < ApplicationController
 
     @adozioni = @adozioni.joins(:libro).where(libro_id: params[:libro_id]) if params[:libro_id].present?
     @adozioni = @adozioni.joins(:scuola).where("import_scuole.id = ?", params[:scuola_id]) if params[:scuola_id].present?
-    @adozioni = @adozioni.find(params[:ids]) if params[:ids].present?
+   
+    #raise params.inspect if params[:ids].present?
+    @adozioni = @adozioni.find(params[:ids].split(",")) if params[:ids].present?
 
   end
 
