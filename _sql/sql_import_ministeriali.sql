1c227999b8a422bd842217101b803b7d17cf93d3
paolotax <paolo.tassinari@gmail.com>
2024-07-07 07:45:08 +0200

    refactor: Update SQL query to include 'categoria' column in ministeriali import

--- _sql/sql_import_ministeriali.sql
+++ _sql/sql_import_ministeriali.sql
@@ -1,9 +1,11 @@
-SELECT DISTINCT new_adozioni.codiceisbn AS codice_isbn,
+SELECT DISTINCT 
+       new_adozioni.codiceisbn AS codice_isbn,
        new_adozioni.titolo,
        editori.id AS editore_id,
        new_adozioni.annocorso as classe,
        new_adozioni.disciplina,
-       COALESCE(TO_NUMBER(new_adozioni.prezzo, 'FM9G999G999D99S'), 0) AS prezzo_in_cents
+       COALESCE(TO_NUMBER(new_adozioni.prezzo, 'FM9G999G999D99S'), 0) AS prezzo_in_cents,
+       'Ministeriali' AS categoria
 FROM new_adozioni
 INNER JOIN new_scuole ON new_adozioni.codicescuola = new_scuole.codice_scuola
 INNER JOIN user_scuole ON new_scuole.import_scuola_id = user_scuole.import_scuola_id
