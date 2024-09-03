1cc1d0778dbfdb79b77b855de2e00ab80d01615a
paolotax <paolo.tassinari@gmail.com>
2024-09-03 23:11:16 +0200

    refactor: Update tax_button_component.rb to include enabled parameter

--- app/components/tax_button_component.rb
+++ app/components/tax_button_component.rb
@@ -4,13 +4,14 @@ class TaxButtonComponent < ViewComponent::Base
   
   attr_reader :caption, :svg_file, :color, :url, :data_attr
   
-  def initialize(caption:, svg_file:, color:, url:, data_attr: {})
+  def initialize(caption:, svg_file:, color:, url:, data_attr: {}, enabled: true)
       
     @caption = caption
     @svg_file = svg_file
     @color = color
     @url = url
     @data_attr = data_attr
+    @enabled = enabled
   end
  
 end
