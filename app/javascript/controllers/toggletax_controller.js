a57e34c0cbcf5ff20fc8684ad3ed79d8d7c6d21d
paolotax <paolo.tassinari@gmail.com>
2024-09-22 08:07:10 +0200

    refactor: Update tax filter form component with improved styling and ToggleTaxButton

--- app/javascript/controllers/toggletax_controller.js
+++ app/javascript/controllers/toggletax_controller.js
@@ -8,13 +8,16 @@ export default class extends Controller {
   toggle() {
 
     this.buttonTarget.classList.toggle("bg-gray-900"); 
-    this.buttonTarget.classList.toggle("bg-gray-200");  
+    this.buttonTarget.classList.toggle("bg-red-500");  
 
     this.pushbuttonTarget.classList.toggle("translate-x-0")
     this.pushbuttonTarget.classList.toggle("translate-x-5");
 
     this.fieldTarget.value === "si" ? this.fieldTarget.value = "no" : this.fieldTarget.value = "si";
-  
+
+    let form = this.element.closest("form")
+    form.requestSubmit();
+
   }
 
 }
