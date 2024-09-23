df71ac345146ebd75c9b33123de81974f02db7e3
paolotax <paolo.tassinari@gmail.com>
2024-09-24 01:39:00 +0200

    Refactor LibriController#index to include Excel export functionality

---
+++ app/javascript/controllers/link_modifier_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+    
+  static targets = ["link"]
+
+    connect() {
+        // Ascolta il caricamento del turbo_frame
+        document.addEventListener('turbo:frame-load', this.modifyLinks.bind(this));
+    }
+
+    modifyLinks(event) {
+        if (event.target.id === "search_results") {
+            var params = window.location.search;
+
+            this.linkTargets.forEach(link => {
+                // Sostituisci o aggiungi i parametri senza duplicarli
+                const baseUrl = link.href.split('?')[0];
+                link.href = baseUrl + params;
+            });
+        }
+    }
+}
+
+
