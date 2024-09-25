30e4735686072068d8a6258b90715204557811e9
paolotax <paolo.tassinari@gmail.com>
2024-09-25 16:56:46 +0200

    Refactor tax_filters_controller.js to use Turbo.visit for form submission

--- app/javascript/controllers/tax_filters_controller.js
+++ app/javascript/controllers/tax_filters_controller.js
@@ -6,7 +6,17 @@ export default class extends Controller {
   static targets = [ "form" ];
 
   submit() {
-    this.formTarget.requestSubmit();
+    // this.formTarget.requestSubmit();
+
+    const form = this.formTarget;
+    // const form = this.element.closest("form")
+    const formData = new FormData(form);
+
+    const params = new URLSearchParams(formData);
+    const newUrl = `${form.action}?${params.toString()}`;
+
+    Turbo.visit(newUrl, { frame: "search_results", action: "advance" });
+
   }
 
 }
