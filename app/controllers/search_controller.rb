afb0c2a2b310715f3c465c0708aef33381eeee75
paolotax <paolo.tassinari@gmail.com>
2024-10-03 16:55:09 +0200

    Refactor search controller to order search results by specific columns

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
-    @import_scuole = current_user.import_scuole.search_all_word(params[:query])
-    @libri = current_user.libri.search_all_word(params[:query])
-    @clienti = current_user.clienti.search(params[:query])
+    @import_scuole = current_user.import_scuole.order(:DENOMINAZIONESCUOLA).search_all_word(params[:query])
+    @libri = current_user.libri.order(:titolo).search_all_word(params[:query])
+    @clienti = current_user.clienti.order(:denominazione).search(params[:query])
   end
 end
