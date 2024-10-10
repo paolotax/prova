721607b7419f12f52eab77ff076e6cce20759dae
paolotax <paolo.tassinari@gmail.com>
2024-10-10 15:48:21 +0200

    Refactor wrapper_css in HeadingComponent to include transparency

--- app/components/heading_component.rb
+++ app/components/heading_component.rb
@@ -7,7 +7,7 @@ class HeadingComponent < ViewComponent::Base
   end
   renders_one :description
 
-  def initialize(level: "h1", wrapper_css: "flex flex-col gap-4 sm:flex-row items-center bg-white sm:justify-between px-4 py-3 mb-4 border border-gray-200 rounded-md shadow-md")
+  def initialize(level: "h1", wrapper_css: "flex flex-col gap-4 sm:flex-row items-center bg-white/90 sm:justify-between px-4 py-3 mb-4 border border-gray-200 rounded-md shadow-md")
     @level, @wrapper_css = level, wrapper_css
 
     raise "Incorrect level; needs to be one of h1, h2, h3, h4" if %w[h1 h2 h3 h4].exclude? @level
