b12f79e015ff486a2e13bb42f0d5d1099188e146
paolotax <paolo.tassinari@gmail.com>
2024-11-15 09:52:36 +0100

    Refactor OpenAI configuration to use environment variable for access token

--- config/initializers/openai.rb
+++ config/initializers/openai.rb
@@ -1,5 +1,5 @@
 require 'openai'
 
 OpenAI.configure do |config|
-  config.access_token = Rails.application.credentials.openai[:api_key]
+  config.access_token = ENV.fetch("OPENAI_ACCESS_TOKEN")
 end
