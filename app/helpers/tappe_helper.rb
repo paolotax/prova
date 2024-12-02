28b1c4e46cc6640b7970af6023af0c2301d6c200
paolotax <paolo.tassinari@gmail.com>
2024-12-02 08:02:02 +0100

    Refactor TappeHelper to include HTML tooltip for scuola information

--- app/helpers/tappe_helper.rb
+++ app/helpers/tappe_helper.rb
@@ -1,5 +1,14 @@
 module TappeHelper
 
+  def html_for_scuola(scuola)
+    content_tag(:div, class: "scuola") do
+      content_tag(:h4, class: "font-semibold hover:font-110%") do
+        link_to(scuola.scuola, scuola)
+      end +
+      content_tag(:p, scuola.indirizzo) +
+      content_tag(:p, "#{scuola.cap} #{scuola.comune} (#{scuola.provincia})")
+    end
+  end
 
   def get_scuole_coordinates(scuole_ids)
     #scuole_ids = current_user.tappe.di_oggi.pluck(:tappable_id)
@@ -12,7 +21,8 @@ module TappeHelper
         latitude: scuola.latitude,
         longitude: scuola.longitude,
         label: scuola.scuola,
-        color: "#f84d4d"
+        color: "#f84d4d",
+        tooltip: html_for_scuola(scuola)
       }
     
     end
