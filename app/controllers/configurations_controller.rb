38805ef260302f220f441856846aa24253adb60f
paolotax <paolo.tassinari@gmail.com>
2025-01-11 16:12:25 +0100

    Update login form to conditionally display remember me checkbox based on Hotwire native app detection
    - Modified the `new.html.erb` view to include a hidden field for `remember_me` when the app is detected as a Hotwire native app, enhancing functionality for native users.
    - Retained the traditional checkbox display for non-native users, ensuring consistent user experience across different app types.
    
    These changes improve the usability of the login form by adapting to the application's environment.

---
+++ app/controllers/configurations_controller.rb
@@ -0,0 +1,70 @@
+#---
+# Excerpted from "Hotwire Native for Rails Developers",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit https://pragprog.com/titles/jmnative for more book information.
+#---
+class ConfigurationsController < ApplicationController
+  def ios_v1
+    render json: {
+      settings: {},
+      rules: [
+        {
+          patterns: [
+            "/new$",
+            "/edit$"
+          ],
+          properties: {
+            context: "modal"
+          }
+        },
+        {
+          patterns: [
+            "/hikes/[0-9]+/map"
+          ],
+          properties: {
+            view_controller: "map"
+          }
+        }
+      ]
+    }
+  end
+
+  def android_v1
+    render json: {
+      settings: {},
+      rules: [
+        {
+          patterns: [
+            ".*"
+          ],
+          properties: {
+            uri: "hotwire://fragment/web",
+            pull_to_refresh_enabled: true
+          }
+        },
+        {
+          patterns: [
+            "/new$",
+            "/edit$"
+          ],
+          properties: {
+            context: "modal",
+            pull_to_refresh_enabled: false
+          }
+        },
+        {
+          patterns: [
+            "/hikes/[0-9]+/map"
+          ],
+          properties: {
+            uri: "hotwire://fragment/map",
+            title: "Map"
+          }
+        }
+      ]
+    }
+  end
+end
