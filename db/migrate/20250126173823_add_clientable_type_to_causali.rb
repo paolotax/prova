d0b103a1b1a7506ed01001ae3c4f2fbfe08fbc4c
paolotax <paolo.tassinari@gmail.com>
2025-01-26 20:06:49 +0100

    Fix migration syntax for adding `clientable_type` to causali table
    - Correct the syntax of `add_column` method in the migration
    - Ensure proper column addition with explicit symbol for column name

--- db/migrate/20250126173823_add_clientable_type_to_causali.rb
+++ db/migrate/20250126173823_add_clientable_type_to_causali.rb
@@ -1,5 +1,5 @@
 class AddClientableTypeToCausali < ActiveRecord::Migration[7.2]
   def change
-    add_column :causali, clientable_type: :string
+    add_column :causali, :clientable_type, :string
   end
 end
