b6aebf3128d31bd4d58a8e4abe4b1bea86b6a9f1
paolotax <paolo.tassinari@gmail.com>
2025-02-17 09:29:45 +0100

    Enhance school search filtering for Giri with excluded schools

--- app/controllers/searches/clientable_controller.rb
+++ app/controllers/searches/clientable_controller.rb
@@ -1,27 +1,29 @@
-module Searches
-  class ClientableController < ApplicationController
-    def index
-      giro_id = params[:giro_id]
-      
-      @results = case params[:type]
-                when 'Cliente'
-                  current_user.clienti.search(params[:query])
-                when 'ImportScuola'
-                  scuole = current_user.import_scuole.search(params[:query])
-                  if giro_id.present?
-                    scuole = current_user.import_scuole.where.not(id: Tappa.where(giro_id: giro_id).where(tappable_type: 'ImportScuola').select(:tappable_id))
-                  end
-                  scuole.search(params[:query]).order(:position)
+class Searches::ClientableController < ApplicationController
+
+  def index
+    giro_id = params[:giro_id]
+    
+    @results = case params[:type]
+              when 'Cliente'
+                current_user.clienti.search(params[:query])
+              when 'ImportScuola'
+                scuole = current_user.import_scuole.search(params[:query])
+                if giro_id.present?
+                  scuole = current_user.import_scuole
+                    .where.not(id: Tappa.where(giro_id: giro_id).where(tappable_type: 'ImportScuola').select(:tappable_id))
+                    .where.not(id: Giro.find(giro_id).excluded_ids)
                 end
+                scuole.search(params[:query]).order(:position)
+              end
 
-      render partial: 'results', locals: { results: @results }
-    end
-
-    # def show
-    # end
-
-    # def new
-      
-    # end
+    render partial: 'results', locals: { results: @results }
   end
+
+  # def show
+  # end
+
+  # def new
+    
+  # end
+
 end
