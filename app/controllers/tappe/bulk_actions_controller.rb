ac91ccc8f2260fc977da08cc02b21e42526840f3
paolotax <paolo.tassinari@gmail.com>
2025-04-03 10:39:40 +0200

    Rimuovi commenti e ripristina la logica di creazione delle Tappe nel controller bulk_actions_controller create

--- app/controllers/tappe/bulk_actions_controller.rb
+++ app/controllers/tappe/bulk_actions_controller.rb
@@ -3,47 +3,47 @@ module Tappe
 
     before_action :authenticate_user!
 
-    # def create
-    #   @tappe = []
-    #   tappable_ids = []
-    #   tappable_type = params[:tappable_type]
+    def create
+      @tappe = []
+      tappable_ids = []
+      tappable_type = params[:tappable_type]
 
-    #   case tappable_type
-    #   when "ImportScuola"
-    #     tappable_ids = current_user.import_scuole.where(id: params[:tappable_ids]).pluck(:id)
-    #   when "Cliente"
-    #     tappable_ids = current_user.clienti.where(id: params[:tappable_ids]).pluck(:id)
-    #   end
+      case tappable_type
+      when "ImportScuola"
+        tappable_ids = current_user.import_scuole.where(id: params[:tappable_ids]).pluck(:id)
+      when "Cliente"
+        tappable_ids = current_user.clienti.where(id: params[:tappable_ids]).pluck(:id)
+      end
 
-    #   new_data_tappa = params[:data_tappa]
-    #   new_titolo     = params[:titolo]
-    #   new_giro_id    = params[:giro_id]
+      new_data_tappa = params[:data_tappa]
+      new_titolo     = params[:titolo]
+      new_giro_id    = params[:giro_id]
 
-    #   tappable_ids.uniq.each do |tappable_id|
-    #     unless tappable_id.blank?
-    #       tappa = current_user.tappe.find_or_create_by(
-    #         tappable_type: tappable_type,
-    #         tappable_id: tappable_id,
-    #         data_tappa: new_data_tappa,
-    #         giro_id: new_giro_id,
-    #         titolo: new_titolo,
-    #         user_id: current_user.id
-    #       )
-    #       @tappe << tappa
-    #     end
-    #   end
+      tappable_ids.uniq.each do |tappable_id|
+        unless tappable_id.blank?
+          tappa = current_user.tappe.find_or_create_by(
+            tappable_type: tappable_type,
+            tappable_id: tappable_id,
+            data_tappa: new_data_tappa,
+            giro_id: new_giro_id,
+            titolo: new_titolo,
+            user_id: current_user.id
+          )
+          @tappe << tappa
+        end
+      end
 
-    #   respond_to do |format|
-    #     format.turbo_stream do
-    #       render turbo_stream: turbo_stream.append("giorno-#{params[:data_tappa]}", 
-    #         partial: "tappe/tappa", 
-    #         collection: @tappe, 
-    #         as: :tappa,
-    #         locals: { with_checkbox: false }
-    #       )
-    #     end
-    #   end
-    # end
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append("giorno-#{params[:data_tappa]}", 
+            partial: "tappe/tappa", 
+            collection: @tappe, 
+            as: :tappa,
+            locals: { with_checkbox: false }
+          )
+        end
+      end
+    end
 
   
     def duplica 
