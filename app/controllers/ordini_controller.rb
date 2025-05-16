6c574c83f1b450ac2b9ee6f707185426e35c5829
paolotax <paolo.tassinari@gmail.com>
2025-05-16 17:59:25 +0200

    Aggiunta della protezione dell'azione 'index' nel controller Ordini per garantire che solo gli utenti autenticati possano accedere alla lista degli ordini.

--- app/controllers/ordini_controller.rb
+++ app/controllers/ordini_controller.rb
@@ -1,6 +1,6 @@
-
-
 class OrdiniController < ApplicationController
+
+  before_action :authenticate_user!
   def index
     @ordini = current_user.righe.joins(:documenti).where("documenti.status = 0")
   end
