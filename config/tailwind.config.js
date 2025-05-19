18cdbd40c62b24738a45e08c1e4fe4afcbd0d628
paolotax <paolo.tassinari@gmail.com>
2025-05-19 09:16:50 +0200

    Tailwind 4

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,14 +1,12 @@
-// config/tailwind.config.js
-
 module.exports = {
   content: [
-    './app/views/**/*.{html,erb}',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
+    "./app/views/**/*.{html,erb}",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/assets/stylesheets/**/*.css"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
