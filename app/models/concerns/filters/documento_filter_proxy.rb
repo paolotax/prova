49d20c41892acd1f041973e055d7b589c86c164f
paolotax <paolo.tassinari@gmail.com>
2025-05-22 18:36:07 +0200

    Refactor document filtering scopes to improve readability and add 'cliente' option for sorting. Clean up whitespace in the filter form for better code consistency.

--- app/models/concerns/filters/documento_filter_proxy.rb
+++ app/models/concerns/filters/documento_filter_proxy.rb
@@ -3,6 +3,8 @@ module Filters
     extend FilterScopeable
 
     filter_scope :search, ->(search) {
+
+    filter_scope :search, ->(search) {
       # joins("LEFT JOIN import_scuole ON documenti.clientable_id = import_scuole.id AND documenti.clientable_type = 'ImportScuola'")
       # .joins("LEFT JOIN clienti ON documenti.clientable_id = clienti.id AND documenti.clientable_type = 'Cliente'")
       joins("JOIN causali ON documenti.causale_id = causali.id")
@@ -13,7 +15,15 @@ module Filters
               OR clienti.comune ILIKE ?
               OR causali.causale ILIKE ?',
       "%#{search}%", "%#{search}%","%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
+      .where('import_scuole."DENOMINAZIONESCUOLA" ILIKE ?
+              OR import_scuole."DESCRIZIONECOMUNE" ILIKE ?
+              OR import_scuole."DENOMINAZIONEISTITUTORIFERIMENTO" ILIKE ?
+              OR clienti.denominazione ILIKE ?
+              OR clienti.comune ILIKE ?
+              OR causali.causale ILIKE ?',
+      "%#{search}%", "%#{search}%","%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%")
     }
+
 
     filter_scope :search_libro, ->(search) { joins(documento_righe: [riga: :libro]).where("libri.titolo ILIKE ?", "%#{search}%").distinct }
     filter_scope :causale, ->(causale) { joins(:causale).where("causali.causale ILIKE ?", "%#{causale}%") }
@@ -24,10 +34,12 @@ module Filters
 
     filter_scope :anno, ->(anno) { where('EXTRACT(YEAR FROM data_documento) = ?', anno) }
 
+
     filter_scope :consegnato_il, ->(data) { where('DATE(consegnato_il) = ?', data) }
     filter_scope :pagato_il, ->(data) { where('DATE(pagato_il) = ?', data.to_date) }
 
     filter_scope :tappe_del_giorno, ->(data) {
+      joins("INNER JOIN tappe ON documenti.clientable_id = tappe.tappable_id
       joins("INNER JOIN tappe ON documenti.clientable_id = tappe.tappable_id
              AND documenti.clientable_type = tappe.tappable_type")
       .where("DATE(tappe.data_tappa) = ?", data)
@@ -37,6 +49,8 @@ module Filters
     def order_by(ordine)
       if ordine == 'fresh'
         unscope(:order).order(Arel.sql('EXTRACT(YEAR FROM data_documento) DESC, created_at DESC'))
+      elsif ordine == 'cliente'
+        unscope(:order).order(Arel.sql('clientable_type DESC, clientable_id DESC, data_documento DESC, numero_documento DESC'))
       elsif ordine == 'cliente'
         unscope(:order).order(Arel.sql('clientable_type DESC, clientable_id DESC, data_documento DESC, numero_documento DESC'))
       else
