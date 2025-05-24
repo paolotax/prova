98e90323f21f13bb9c24a008340264b90f68dae0
paolotax <paolo.tassinari@gmail.com>
2025-05-24 07:57:50 +0200

    documenti in consegna nel giorno

--- app/controllers/documenti_controller.rb
+++ app/controllers/documenti_controller.rb
@@ -8,7 +8,7 @@ class DocumentiController < ApplicationController
   def index
     @causali = Causale.all
     @import = DocumentiImporter.new
-    
+
     @documenti = current_user.documenti
         .joins("left outer join import_scuole on documenti.clientable_type = 'ImportScuola' and documenti.clientable_id = import_scuole.id")
         .joins("left outer join clienti on documenti.clientable_type = 'Cliente' and documenti.clientable_id = clienti.id")
@@ -16,11 +16,11 @@ class DocumentiController < ApplicationController
         .order(data_documento: :desc, causale_id: :desc, numero_documento: :desc)
 
     @documenti = filter(@documenti.all)
-    
+
     @pagy, @documenti = pagy(@documenti.all, items: 20)
-    
+
     respond_to do |format|
-      format.html 
+      format.html
       format.turbo_stream
       format.xlsx
     end
@@ -33,11 +33,11 @@ class DocumentiController < ApplicationController
         pdf = DocumentoPdf.new(@documento, view_context)
         send_data pdf.render, filename: "documento_#{@documento.id}.pdf",
                               type: "application/pdf",
-                              disposition: "inline"      
+                              disposition: "inline"
       end
     end
   end
-  
+
   def new
     causale = Causale.find_by(causale: params[:causale])
     clientable_id = params[:clientable_id]
@@ -46,11 +46,11 @@ class DocumentiController < ApplicationController
                         .where(causale: causale)
                         .where('EXTRACT(YEAR FROM data_documento) = ?', Date.today.year)
                         .maximum(:numero_documento) || 0).to_i + 1
-    
+
     @documento = current_user.documenti.build(numero_documento: numero_documento, data_documento: Date.today, causale: causale, clientable_id: clientable_id, clientable_type: clientable_type)
     @documento.save! validate: false
-    @documento.documento_righe.build.build_riga(sconto: 0.0)  
-  
+    @documento.documento_righe.build.build_riga(sconto: 0.0)
+
     redirect_to documento_step_path(@documento, Documento.form_steps.keys.first)
   end
 
@@ -87,43 +87,43 @@ class DocumentiController < ApplicationController
 
   def destroy
     @documento.destroy!
-  
+
     respond_to do |format|
-      format.turbo_stream do 
+      format.turbo_stream do
         flash.now[:notice] = "Documento eliminato."
         render turbo_stream: turbo_stream.remove(@documento)
-      end 
+      end
       format.html { redirect_to documenti_url, notice: "Documento eliminato.", status: :see_other }
     end
   end
 
 
   def nuovo_numero_documento
-    
+
     causale = Causale.find(params[:causale])
-    
+
     clientable_type = causale.clientable_type&.camelize || "Cliente"
-    
+
 
     anno_corrente = Date.current.year
     ultimo_documento = current_user.documenti.where(causale: params[:causale]).where("EXTRACT(YEAR FROM data_documento) = ?", anno_corrente).maximum(:numero_documento)
     numero_documento = (ultimo_documento || 0) + 1
     render json: { numero_documento: numero_documento, clientable_type: clientable_type }
-  
+
   end
 
-  def filtra 
+  def filtra
     @causali = Causale.all
   end
 
   def esporta_xml
     @documento = Documento.find(params[:id])
     xml_generator = FatturaElettronicaXml.new(@documento)
-    
+
     respond_to do |format|
       format.xml do
         xml_content = xml_generator.genera_xml
-        send_data xml_content, 
+        send_data xml_content,
                   filename: "IT#{@documento.user.azienda_partita_iva}_#{@documento.numero_documento}.xml",
                   type: 'application/xml',
                   disposition: 'attachment'
@@ -138,13 +138,13 @@ class DocumentiController < ApplicationController
     end
 
     def documento_params
-      params.require(:documento).permit(:cliente_id, :clientable_id, :clientable_type, :referente, :note, :numero_documento, :user_id, :data_documento, :causale_id, :tipo_pagamento, :consegnato_il, :pagato_il, :status, :iva_cents, :totale_cents, :spese_cents, :totale_copie, :tipo_documento, 
-        documento_righe_attributes: [:id, :posizione, 
+      params.require(:documento).permit(:cliente_id, :clientable_id, :clientable_type, :referente, :note, :numero_documento, :user_id, :data_documento, :causale_id, :tipo_pagamento, :consegnato_il, :pagato_il, :status, :iva_cents, :totale_cents, :spese_cents, :totale_copie, :tipo_documento,
+        documento_righe_attributes: [:id, :posizione,
           { riga_attributes: [ :id, :libro_id, :quantita, :prezzo, :prezzo_cents, :prezzo_copertina_cents, :sconto, :iva_cents, :status, :_destroy] }
         ])
     end
 
-    def filter_params 
+    def filter_params
       {
         search: params["search"],
         search_libro: params["search_libro"],
@@ -153,6 +153,8 @@ class DocumentiController < ApplicationController
         tipo_pagamento: params["tipo_pagamento"],
         anno: params["anno"],
         ordina_per: params["ordina_per"],
+        consegnato_il: params["consegnato_il"],
+        pagato_il: params["pagato_il"],
         tappe_del_giorno: params["tappe_del_giorno"]
       }.compact_blank
     end
