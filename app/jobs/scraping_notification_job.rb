c295534ecc5a377b38307e0072f0dfb7b8028dbe
paolotax <paolo.tassinari@gmail.com>
2025-06-09 20:50:10 +0200

    Refactor ScrapingNotificationJob to use Sidekiq and update production mailer host to scagnozz.com

--- app/jobs/scraping_notification_job.rb
+++ app/jobs/scraping_notification_job.rb
@@ -1,5 +1,5 @@
-class ScrapingNotificationJob < ApplicationJob
-  queue_as :default
+class ScrapingNotificationJob
+  include Sidekiq::Job
 
   def perform(regioni_aggiornate, regioni_saltate, regioni_nuove)
     ScrapingMailer.scraping_completed(regioni_aggiornate, regioni_saltate, regioni_nuove).deliver_now
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "scagnozz.com" }
 
   # Configure email delivery using Resend
   config.action_mailer.delivery_method = :resend
