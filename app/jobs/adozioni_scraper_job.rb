771ea421615dc31d08e75b4112ab1de092d17f97
paolotax <paolo.tassinari@gmail.com>
2025-06-10 21:12:39 +0200

    Enable re-invocation of the Rake task in AdozioniScraperJob for improved task execution

--- app/jobs/adozioni_scraper_job.rb
+++ app/jobs/adozioni_scraper_job.rb
@@ -5,6 +5,8 @@ class AdozioniScraperJob
 
   def perform
     Rails.logger.info "Inizio scraping adozioni..."
+    Rails.application.load_tasks
+    Rake::Task['scrape:adozioni'].reenable
     Rake::Task['scrape:adozioni'].invoke
     Rails.logger.info "Scraping adozioni completato"
   end
