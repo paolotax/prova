828de502c3475fb9beb91b5d54fb833949743302
paolotax <paolo.tassinari@gmail.com>
2025-06-10 05:35:32 +0200

    schedule

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,16 @@
 require 'sidekiq'
 require 'sidekiq-scheduler'
 
-Sidekiq.configure_server do |config|
-  config.logger = Rails.logger
+config.on(:startup) do
+  schedule_file = "config/schedule.yml"
 
-  config.on(:startup) do
-    schedule_file = "config/schedule.yml"
-    if File.exist?(schedule_file)
-      Sidekiq.schedule = YAML.load_file(schedule_file)
-      SidekiqScheduler::Scheduler.instance.reload_schedule!
-      Rails.logger.info("Sidekiq schedule loaded from #{schedule_file}")
-    else
-      Rails.logger.warn("Sidekiq schedule file not found: #{schedule_file}")
-    end
+  if File.exist?(schedule_file)
+    schedule = YAML.load_file(schedule_file)
+    SidekiqScheduler::Scheduler.dynamic = true
+    SidekiqScheduler::Scheduler.instance.load_schedule!(schedule)
+    Rails.logger.info("Sidekiq schedule loaded from #{schedule_file}")
+  else
+    Rails.logger.warn("Sidekiq schedule file not found: #{schedule_file}")
   end
 end
 
