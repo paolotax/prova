11973c543e96f49cd10b4021b5b9e7a30ec3ced1
paolotax <paolo.tassinari@gmail.com>
2025-06-10 05:30:17 +0200

    Add Sidekiq schedule configuration from YAML file and define ScrapeAdozioniJob schedule

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,10 +3,19 @@ require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
   config.logger = Rails.logger
+
   config.on(:startup) do
-    SidekiqScheduler::Scheduler.instance.reload_schedule!
+    schedule_file = "config/schedule.yml"
+    if File.exist?(schedule_file)
+      Sidekiq.schedule = YAML.load_file(schedule_file)
+      SidekiqScheduler::Scheduler.instance.reload_schedule!
+      Rails.logger.info("Sidekiq schedule loaded from #{schedule_file}")
+    else
+      Rails.logger.warn("Sidekiq schedule file not found: #{schedule_file}")
+    end
   end
 end
+
 
 Sidekiq.configure_client do |config|
   config.logger = Rails.logger
