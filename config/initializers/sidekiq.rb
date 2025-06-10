7f0d9130c73e5442b0868439762e3361ad8246a3
paolotax <paolo.tassinari@gmail.com>
2025-06-10 05:22:59 +0200

    Remove loading of Sidekiq schedule from YAML file on server startup

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,7 +4,6 @@ require 'sidekiq-scheduler'
 Sidekiq.configure_server do |config|
   config.logger = Rails.logger
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq.yml', __FILE__))
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
 end
