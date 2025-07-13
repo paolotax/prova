0bb7179c428562c83e8b5094f1e077db06a28f60
paolotax <paolo.tassinari@gmail.com>
2025-07-13 11:14:12 +0200

    nvim

--- app/models/appunto.rb
+++ app/models/appunto.rb
@@ -29,11 +29,10 @@
 #
 
 class Appunto < ApplicationRecord
-  
   belongs_to :import_scuola, required: false
   belongs_to :user
   belongs_to :import_adozione, required: false
-  belongs_to :classe, class_name: "Views::Classe", optional: true
+  belongs_to :classe, class_name: 'Views::Classe', optional: true
 
   has_one_attached :image
   has_many_attached :attachments
@@ -43,83 +42,85 @@ class Appunto < ApplicationRecord
 
   belongs_to :voice_note, optional: true
 
-
   include PgSearch::Model
 
-  search_fields =  [ :nome, :body, :email, :telefono, :stato]
+  search_fields = %i[nome body email telefono stato]
 
-  pg_search_scope :search_all_word, 
-                      against: search_fields,
-                      associated_against: {
-                        import_scuola:   [:CODICESCUOLA, :DENOMINAZIONESCUOLA, :DESCRIZIONECOMUNE],
-                        import_adozione: [:CODICESCUOLA, :CODICEISBN, :EDITORE],
-                        rich_text_content: [:body],
-                        attachments_blobs: [:filename]
-                      },
-                      using: {
-                        tsearch: { any_word: false, prefix: true }
-                      }
+  pg_search_scope :search_all_word,
+                  against: search_fields,
+                  associated_against: {
+                    import_scuola: %i[CODICESCUOLA DENOMINAZIONESCUOLA DESCRIZIONECOMUNE],
+                    import_adozione: %i[CODICESCUOLA CODICEISBN EDITORE],
+                    rich_text_content: [:body],
+                    attachments_blobs: [:filename]
+                  },
+                  using: {
+                    tsearch: { any_word: false, prefix: true }
+                  }
 
   include Searchable
 
-  search_on :nome, :body, :email, :telefono, :stato, 
-            import_adozione: [:CODICESCUOLA, :CODICEISBN, :EDITORE],
+  search_on :nome, :body, :email, :telefono, :stato,
+            import_adozione: %i[CODICESCUOLA CODICEISBN EDITORE],
             rich_text_content: [:body],
-            attachments_blobs: [:filename], 
-            import_scuola: [:CODICESCUOLA, :DENOMINAZIONESCUOLA, :DESCRIZIONECOMUNE, :DESCRIZIONECARATTERISTICASCUOLA, :DESCRIZIONETIPOLOGIAGRADOISTRUZIONESCUOLA, :CODICEISTITUTORIFERIMENTO, :DENOMINAZIONEISTITUTORIFERIMENTO]
-  
+            attachments_blobs: [:filename],
+            import_scuola: %i[CODICESCUOLA DENOMINAZIONESCUOLA DESCRIZIONECOMUNE DESCRIZIONECARATTERISTICASCUOLA DESCRIZIONETIPOLOGIAGRADOISTRUZIONESCUOLA CODICEISTITUTORIFERIMENTO DENOMINAZIONEISTITUTORIFERIMENTO]
+
   extend FilterableModel
   class << self
     def filter_proxy = Filters::AppuntoFilterProxy
   end
 
-  STATO_APPUNTI = ["da fare", "in evidenza", "in settimana", "in visione", "da pagare", "completato", "archiviato"]  
-  
-  FILTERS = [ 
-    ["Tutti",  "/appunti"], 
-    ["Oggi",   "/appunti?filter=oggi"], 
-    ["Domani", "/appunti?filter=domani"],
-    ["In sospeso", "/appunti?stato=in_sospeso"],
-    ["In settimana", "/appunti?stato=in_settimana"],
-    ["Non archiviati", "/appunti?filter=non_archiviati"],
-    ["Archiviati", "/appunti?stato=archiviato"],
+  STATO_APPUNTI = ['da fare', 'in evidenza', 'in settimana', 'in visione', 'da pagare', 'completato', 'archiviato']
+
+  FILTERS = [
+    ['Tutti',  '/appunti'],
+    ['Oggi',   '/appunti?filter=oggi'],
+    ['Domani', '/appunti?filter=domani'],
+    ['In sospeso', '/appunti?stato=in_sospeso'],
+    ['In settimana', '/appunti?stato=in_settimana'],
+    ['Non archiviati', '/appunti?filter=non_archiviati'],
+    ['Archiviati', '/appunti?stato=archiviato']
   ]
 
-  
-  after_initialize :set_default_stato, :if => :new_record?
-  
+  after_initialize :set_default_stato, if: :new_record?
+
   def set_default_stato
-    self.stato ||= "da fare"
+    self.stato ||= 'da fare'
   end
 
   scope :dell_utente, ->(user) { where(user_id: user.id) }
 
-  scope :non_saggi, -> { where.not(nome: ["saggio", "seguito", "kit"]) }
-  
-  scope :da_fare, -> { where(stato: "da fare").non_saggi }
-  scope :in_evidenza, -> { where(stato: "in evidenza").non_saggi }
-  scope :in_settimana, -> { where(stato: "in settimana").non_saggi }
-  scope :da_pagare, -> { where(stato: "da pagare").non_saggi }
-  scope :in_visione, -> { where(stato: "in visione").non_saggi }
-  scope :completati, -> { where(stato: "completato").non_saggi }
-  
-  scope :archiviati, -> { where(stato: "archiviato").non_saggi }
- 
-  scope :da_completare, -> { where(stato: ["da fare", "in evidenza", "in settimana"]).non_saggi }
-  scope :in_sospeso, -> { where(stato: ["in visione", "da pagare"]).non_saggi } 
-  scope :non_archiviati, -> { where.not(stato: ["archiviato", "completato"]).non_saggi }
+  scope :non_saggi, -> { where.not(nome: %w[saggio seguito kit]) }
+
+  scope :da_fare, -> { where(stato: 'da fare').non_saggi }
+  scope :in_evidenza, -> { where(stato: 'in evidenza').non_saggi }
+  scope :in_settimana, -> { where(stato: 'in settimana').non_saggi }
+  scope :da_pagare, -> { where(stato: 'da pagare').non_saggi }
+  scope :in_visione, -> { where(stato: 'in visione').non_saggi }
+  scope :completati, -> { where(stato: 'completato').non_saggi }
+
+  scope :archiviati, -> { where(stato: 'archiviato').non_saggi }
+
+  scope :da_completare, -> { where(stato: ['da fare', 'in evidenza', 'in settimana']).non_saggi }
+  scope :in_sospeso, -> { where(stato: ['in visione', 'da pagare']).non_saggi }
+  scope :non_archiviati, -> { where.not(stato: %w[archiviato completato]).non_saggi }
 
   # non includono clienti REFACTOR appunto clientable
-  scope :nel_baule_di_oggi, -> { where(import_scuola_id: Current.user.tappe.di_oggi.where(tappable_type: "ImportScuola").pluck(:tappable_id)) }  
-  scope :nel_baule_di_domani, -> { where(import_scuola_id: Current.user.tappe.di_domani.where(tappable_type: "ImportScuola").pluck(:tappable_id)) }  
-  scope :nel_baule_del_giorno, ->(day) { where(import_scuola_id: Current.user.tappe.del_giorno(day).where(tappable_type: "ImportScuola").pluck(:tappable_id)) }  
+  scope :nel_baule_di_oggi, lambda {
+    where(import_scuola_id: Current.user.tappe.di_oggi.where(tappable_type: 'ImportScuola').pluck(:tappable_id))
+  }
+  scope :nel_baule_di_domani, lambda {
+    where(import_scuola_id: Current.user.tappe.di_domani.where(tappable_type: 'ImportScuola').pluck(:tappable_id))
+  }
+  scope :nel_baule_del_giorno, lambda { |day|
+    where(import_scuola_id: Current.user.tappe.del_giorno(day).where(tappable_type: 'ImportScuola').pluck(:tappable_id))
+  }
 
-
-  scope :saggi, -> { where(nome: "saggio").where.not(import_adozione_id: nil) }
-  scope :seguiti, -> { where(nome: "seguito").where.not(import_adozione_id: nil) }
-  scope :kit, -> { where(nome: "kit").where.not(import_adozione_id: nil) }
-  scope :ssk, -> { where(nome: ["saggio", "seguito", "kit"]).where.not(import_adozione_id: nil) }
-  
+  scope :saggi, -> { where(nome: 'saggio').where.not(import_adozione_id: nil) }
+  scope :seguiti, -> { where(nome: 'seguito').where.not(import_adozione_id: nil) }
+  scope :kit, -> { where(nome: 'kit').where.not(import_adozione_id: nil) }
+  scope :ssk, -> { where(nome: %w[saggio seguito kit]).where.not(import_adozione_id: nil) }
 
   def self.ransackable_attributes(auth_object = nil)
     %w[nome body]
@@ -129,33 +130,33 @@ class Appunto < ApplicationRecord
     %w[nome body]
   end
 
-  def scuola 
+  def scuola
     import_scuola
   end
-  
+
   def content_to_s
-    content.to_s.gsub('<div class="trix-content">', "")
-        .gsub('</div>', "")
-        .gsub('<div>', "")
-        .gsub('</br>', "")
-        .gsub('<p>', "")
-        .gsub('</p>', "")
-        .gsub('<div>', "")
-        .gsub('</div>', "")
-        .gsub('<br/>', "")
-        .gsub('</br/>', "")
-        .gsub('<p/>', "")
-        .gsub('</p/>', "")
-        .gsub('<div/>', "")
-        .gsub('</div/>', "")
-        .gsub('<br />', "")
-        .gsub('</br />', "")
-        .gsub('<p />', "")
-        .gsub('</p />', "")
-        .gsub('<div />', "")
-        .gsub('</div />', "")
+    content.to_s.gsub('<div class="trix-content">', '')
+           .gsub('</div>', '')
+           .gsub('<div>', '')
+           .gsub('</br>', '')
+           .gsub('<p>', '')
+           .gsub('</p>', '')
+           .gsub('<div>', '')
+           .gsub('</div>', '')
+           .gsub('<br/>', '')
+           .gsub('</br/>', '')
+           .gsub('<p/>', '')
+           .gsub('</p/>', '')
+           .gsub('<div/>', '')
+           .gsub('</div/>', '')
+           .gsub('<br />', '')
+           .gsub('</br />', '')
+           .gsub('<p />', '')
+           .gsub('</p />', '')
+           .gsub('<div />', '')
+           .gsub('</div />', '')
   end
-  
+
   def nome_e_classe
     if nome.present? && classe.present?
       "#{nome} - #{classe.to_combobox_display}"
@@ -164,15 +165,15 @@ class Appunto < ApplicationRecord
     elsif classe.present?
       classe.to_combobox_display
     else
-      ""
+      ''
     end
   end
-  
+
   def image_as_thumbnail
     return unless image.content_type.in?(%w[image/jpeg image/png image/jpg image/gif image/webp])
-    self.image.variant(resize_to_limit: [300, 300]).processed
-  end
 
+    image.variant(resize_to_limit: [300, 300]).processed
+  end
 
   def appunto_attachment(index)
     target = attachments[index]
@@ -187,11 +188,9 @@ class Appunto < ApplicationRecord
 
   def representables_attachments
     representables_attachments = []
-    if self.attachments.attached?
-      self.attachments.each do |att|
-        if att.representable?
-          representables_attachments << att
-        end   
+    if attachments.attached?
+      attachments.each do |att|
+        representables_attachments << att if att.representable?
       end
     end
     representables_attachments
@@ -199,36 +198,33 @@ class Appunto < ApplicationRecord
 
   def file_attachments
     file_attachments = []
-    if self.attachments.attached?
-      self.attachments.each do |att|
-        if !att.representable?
-          file_attachments << att
-        end   
+    if attachments.attached?
+      attachments.each do |att|
+        file_attachments << att unless att.representable?
       end
     end
     file_attachments
   end
 
   def self.nel_baule
-    appunti_scuole_di_oggi =  Appunto.where(import_scuola_id: Tappa.di_oggi.where(tappable_type: "ImportScuola").pluck(:tappable_id))
-    #appunti_adozioni_di_oggi = Appunto.where(import_adozione_id: Tappa.di_oggi.where(tappable_type: "ImportAdozione").pluck(:tappable_id)) 
-    #appunti_scuole_di_oggi.or(appunti_adozioni_di_oggi)
+    appunti_scuole_di_oggi = Appunto.where(import_scuola_id: Tappa.di_oggi.where(tappable_type: 'ImportScuola').pluck(:tappable_id))
+    # appunti_adozioni_di_oggi = Appunto.where(import_adozione_id: Tappa.di_oggi.where(tappable_type: "ImportAdozione").pluck(:tappable_id))
+    # appunti_scuole_di_oggi.or(appunti_adozioni_di_oggi)
   end
 
   def is_saggio?
-    nome == "saggio" && self.import_adozione.present?
+    nome == 'saggio' && import_adozione.present?
   end
 
   def is_seguito?
-    nome == "seguito" && self.import_adozione.present?
+    nome == 'seguito' && import_adozione.present?
   end
 
   def is_kit?
-    nome == "seguito" && self.import_adozione.present?
+    nome == 'seguito' && import_adozione.present?
   end
 
   def is_ssk?
-    ["saggio", "seguito", "kit"].include?(nome) && self.import_adozione.present?
+    %w[saggio seguito kit].include?(nome) && import_adozione.present?
   end
-
 end
