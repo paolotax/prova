ed497aeb2de49f13f2104e064a024526675ae491
paolotax <paolo.tassinari@gmail.com>
2025-09-14 09:41:15 +0200

    Refactor SQL user ID substitution in Stat model for improved readability

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -37,8 +37,8 @@ class Stat < ApplicationRecord
 
     def execute(user)
         sql = self.testo
-        sql.gsub!("{{user.id}}", user.id.to_s)
-        sql.gsub!(":user_id", user.id.to_s)
+        sql.gsub!(/\{\{user.id\}\}|:user_id/, user.id.to_s)
+
         ActiveRecord::Base.connection.execute(sql)
     end
 
