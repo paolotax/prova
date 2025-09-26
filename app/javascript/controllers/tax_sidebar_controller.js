463dd8a85fd288d9037be47c89ced399b8fa3e00
paolotax <paolo.tassinari@gmail.com>
2025-09-26 16:40:44 +0200

    fix

--- app/javascript/controllers/tax_sidebar_controller.js
+++ app/javascript/controllers/tax_sidebar_controller.js
@@ -4,18 +4,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
 
   static targets = ["element"];
-  static values = { open: Boolean };
 
   connect() {
     console.log("tax-sidebar connected");
-    // Restore previous state from sessionStorage
-    this.restoreState();
-  }
-
-  disconnect() {
-    console.log("tax-sidebar disconnected");
-    // Save current state before disconnect
-    this.saveState();
   }
 
   toggle(event) {
@@ -24,26 +15,6 @@ export default class extends Controller {
 
     this.elementTargets.forEach((element) => {
       element.classList.toggle("hidden");
-    });
-
-    // Save state after toggle
-    this.saveState();
-  }
-
-  saveState() {
-    const isOpen = this.elementTargets.some(el => !el.classList.contains("hidden"));
-    sessionStorage.setItem("tax-sidebar-open", isOpen.toString());
-  }
-
-  restoreState() {
-    const wasOpen = sessionStorage.getItem("tax-sidebar-open") === "true";
-
-    this.elementTargets.forEach((element) => {
-      if (wasOpen) {
-        element.classList.remove("hidden");
-      } else {
-        element.classList.add("hidden");
-      }
     });
   }
 }
