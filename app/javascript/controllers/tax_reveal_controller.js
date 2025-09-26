81a56df96ebdf98e51beb62e2a8b9f3ba649fe90
paolotax <paolo.tassinari@gmail.com>
2025-09-26 16:16:49 +0200

    tax_reveal e tax_sidebar fix

--- app/javascript/controllers/tax_reveal_controller.js
+++ app/javascript/controllers/tax_reveal_controller.js
@@ -1,16 +1,39 @@
 import Reveal from '@stimulus-components/reveal'
 
 export default class extends Reveal {
-  
+
   static targets = ["chevron"];
 
   connect() {
     super.connect()
+    this.restoreState()
+  }
+
+  disconnect() {
+    this.saveState()
+    super.disconnect()
   }
 
   toggle(e) {
     super.toggle()
     e.preventDefault()
     this.chevronTarget.classList.toggle('rotate-90')
+    this.saveState()
+  }
+
+  saveState() {
+    const sectionId = this.element.closest('li')?.querySelector('a')?.href?.split('/').pop() || 'unknown'
+    const isOpen = !this.itemTarget.classList.contains('hidden')
+    sessionStorage.setItem(`tax-reveal-${sectionId}`, isOpen.toString())
+  }
+
+  restoreState() {
+    const sectionId = this.element.closest('li')?.querySelector('a')?.href?.split('/').pop() || 'unknown'
+    const wasOpen = sessionStorage.getItem(`tax-reveal-${sectionId}`) === 'true'
+
+    if (wasOpen && this.itemTarget.classList.contains('hidden')) {
+      this.itemTarget.classList.remove('hidden')
+      this.chevronTarget.classList.add('rotate-90')
+    }
   }
 }
--- app/javascript/controllers/tax_sidebar_controller.js
+++ app/javascript/controllers/tax_sidebar_controller.js
@@ -8,6 +8,14 @@ export default class extends Controller {
 
   connect() {
     console.log("tax-sidebar connected");
+    // Restore previous state from sessionStorage
+    this.restoreState();
+  }
+
+  disconnect() {
+    console.log("tax-sidebar disconnected");
+    // Save current state before disconnect
+    this.saveState();
   }
 
   toggle(event) {
@@ -16,6 +24,26 @@ export default class extends Controller {
 
     this.elementTargets.forEach((element) => {
       element.classList.toggle("hidden");
+    });
+
+    // Save state after toggle
+    this.saveState();
+  }
+
+  saveState() {
+    const isOpen = this.elementTargets.some(el => !el.classList.contains("hidden"));
+    sessionStorage.setItem("tax-sidebar-open", isOpen.toString());
+  }
+
+  restoreState() {
+    const wasOpen = sessionStorage.getItem("tax-sidebar-open") === "true";
+
+    this.elementTargets.forEach((element) => {
+      if (wasOpen) {
+        element.classList.remove("hidden");
+      } else {
+        element.classList.add("hidden");
+      }
     });
   }
 }
