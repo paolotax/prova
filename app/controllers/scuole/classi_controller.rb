004a67a8f10a1e4b495c0e4d68a539505ab86372
paolotax <paolo.tassinari@gmail.com>
2026-06-28 22:31:34 +0200

    fix(passaggio-anno): navigazione prev/next classe salta le archiviate

--- app/controllers/scuole/classi_controller.rb
+++ app/controllers/scuole/classi_controller.rb
@@ -74,7 +74,7 @@ module Scuole
     end
 
     def load_prev_next
-      all_ids = @scuola.classi.order(:anno_corso, :sezione).pluck(:id)
+      all_ids = @scuola.classi.attive.order(:anno_corso, :sezione).pluck(:id)
       idx = all_ids.index(@classe.id)
       @prev_classe_id = idx && idx > 0 ? all_ids[idx - 1] : nil
       @next_classe_id = idx && idx < all_ids.size - 1 ? all_ids[idx + 1] : nil
